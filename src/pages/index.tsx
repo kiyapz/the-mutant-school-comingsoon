5177771729cd42df8bc883c59af19c09c5f56e70
rihanvyakoob07 <rihanvy32@gmail.com>
2025-06-01 19:29:20 +0530

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -39,7 +39,7 @@ export default function Index() {
         
         {/* Favicon */}
         <link rel="icon" href="/favicon.ico" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/The Mutant School.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         
         {/* Performance */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
